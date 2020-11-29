025e49f1b1fbc4ae183f6cb9932366150922c617
yashmunjal <yashmu45@gmail.com>
2020-11-29 16:28:41 +0530

    courses-view

--- routes/courses.js
+++ routes/courses.js
@@ -3,7 +3,7 @@ const Course=require('../models/course');
 module.exports=function(app){
     app.get('/courses',function(req,res,next){
         Course.find({},function(err,courses){
-            res.render('course/courses',{courses:courses})
+            res.render('course/courses',{courses:courses,name:req.user.email})
         })
     })
 }
