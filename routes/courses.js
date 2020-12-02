ac22f8a1645d489319d78e9b66a0fd96f8b7b730
yashmunjal <yashmu45@gmail.com>
2020-12-02 23:20:48 +0530

    courseDesc\

--- routes/courses.js
+++ routes/courses.js
@@ -1,4 +1,6 @@
 const Course=require('../models/course');
+const User=require('../models/userModel');
+var async = require('async');
 
 module.exports=function(app){
     app.get('/courses',function(req,res,next){
@@ -17,11 +19,30 @@ module.exports=function(app){
                     
                 })
             },
-        ], function(err,results){
+            function(callback){
+                User.findOne({_id:req.user._id,'coursesTaken.course':req.params.id})
+                .populate('coursesTaken.course')
+                .exec(function(err,foundUserCourse){
+                    callback(err,foundUserCourse)
+                    
+                })
+            },
+            function(callback){
+                Course.find({_id:req.params.id},function(err,courseFound){
+                    callback(err,courseFound)
+                    
+                })
+            },
+        ], async function(err,results){
             var course=results[0];
+            
+            var teacherName;
+            await User.find({_id:course[0].ownByTeacher},function(err,teacherfound){
+                teacherName=teacherfound[0].email;
+                
+            })
 
-
-            res.render('course/courseDesc',{courses:course,name:req.user.email})
+            res.render('course/courseDesc',{courses:course,name:req.user.email,teacherName:teacherName})
         })
 
         
