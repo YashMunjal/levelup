506090ab4461df07e6537f175be16dcce7c18183
yashmunjal <yashmu45@gmail.com>
2020-12-02 19:58:25 +0530

    courseDesc

--- routes/courses.js
+++ routes/courses.js
@@ -3,7 +3,27 @@ const Course=require('../models/course');
 module.exports=function(app){
     app.get('/courses',function(req,res,next){
         Course.find({},function(err,courses){
+            //console.log(courses);
             res.render('course/courses',{courses:courses,name:req.user.email})
         })
+    })
+
+    app.get('/courses/:id',function(req,res,next){
+
+        async.parallel([
+            function(callback){
+                Course.find({_id:req.params.id},function(err,courseFound){
+                    callback(err,courseFound)
+                    
+                })
+            },
+        ], function(err,results){
+            var course=results[0];
+
+
+            res.render('course/courseDesc',{courses:course,name:req.user.email})
+        })
+
+        
     })
 }
